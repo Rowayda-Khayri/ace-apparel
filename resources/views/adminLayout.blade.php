29e7a33d30173b2e25edcf1868a455f007806e29
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-11-01 12:35:47 +0200

    create adminLayout

---
+++ resources/views/adminLayout.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+        
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                
+                <div style="margin-right: 50px;color: whitesmoke;">
+                    <h3>Admin Dashboard</h3>
+                </div>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+            </div>
+            
+            
+            
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    
+                    <li>
+                        <a href="" style="font-size: 17px;">Contact Info</a>
+                    </li>
+                    <li>
+                        <a href="" style="font-size: 17px;">Slider</a>
+                    </li>
+                    
+                    
+                    <li>
+                        <a href="" style="font-size: 17px;">Blog Categories</a>
+                    </li>
+                    <li>
+                        <a href="" style="font-size: 17px;">Blog Posts</a>
+                    </li>
+                    <li>
+                        <a href="" style="font-size: 17px;">Authors</a>
+                    </li>
+                    
+                    
+                    <li>
+                        <a href="" style="font-size: 17px;">Messages</a>
+                    </li>
+                    
+                </ul>
+                
+            </div>
+            
+            
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div  class="col-lg-12" style="margin-top: 100px;">
+                
+                @yield('pageContent')
+                
+               
+                
+                
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <!--<script src="js/jquery.js"></script>-->
+
+    <!-- Bootstrap Core JavaScript -->
+    <!--<script src="js/bootstrap.min.js"></script>-->
+
+    
+</body>
+
+</html>
+
+
