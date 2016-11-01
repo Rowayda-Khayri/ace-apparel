76d6c74878bf54d8873c8a7235682df46dcd4af5
Rowayda Khayri <rowayda_khayri@yahoo.com>
2016-11-01 17:07:24 +0200

    try to drop author_id column in posts table : fail

---
+++ database/migrations/2016_11_01_143935_make_category_id_fk_and_drop_author_id_in_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeCategoryIdFkAndDropAuthorIdInPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            
+            $table->dropForeign('posts_author_id_foreign');
+            $table->dropColumn('author_id');
+        });
+    }
+}
